6573a784391fa0fe93de5744921f358766b3d8b3
sheetaldhumal <sheet.dhumal5@gmail.com>
2019-12-03 16:32:38 +0530

    test commit for codeigniter

---
+++ application/views/footer.php
@@ -0,0 +1,33 @@
+					<style>
+                        #response{display: none}
+                        div #fb, div #gp, div #tw{display: inline-block;}
+                        #fb{width: 180px;}
+                        #gp{width:  100px;}
+                        #tw{width: 180px;}
+                    </style>
+					<div id="fb-root"></div>
+					<script>(function(d, s, id) {
+					  var js, fjs = d.getElementsByTagName(s)[0];
+					  if (d.getElementById(id)) return;
+					  js = d.createElement(s); js.id = id;
+					  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.7";
+					  fjs.parentNode.insertBefore(js, fjs);
+					}(document, 'script', 'facebook-jssdk'));</script>
+                    <div>
+					 <div id="tw">
+                            <a href="https://twitter.com/trycatchclasses" class="twitter-follow-button" data-show-count="false" data-size="medium">Follow @trycatchclasses</a>
+                            <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+                        </div>
+                        <div id="gp">
+                            <!-- Place this tag in your head or just before your close body tag. -->
+                           <script src="https://apis.google.com/js/platform.js" async defer></script>
+                           <!-- Place this tag where you want the +1 button to render. -->
+                           <div class="g-plusone" data-href="https://plus.google.com/+TryCatchClassesMumbai"></div>
+                       </div>
+                        <div id="fb">
+							<div class="fb-like" data-href="https://www.facebook.com/TryCatchClasses/" data-layout="standard" data-action="like" data-size="small" data-show-faces="true" data-share="true"></div>
+						</div>
+                       
+                    </div>
+    </body>
+</html>
---
+++ application/views/view_home.php
@@ -0,0 +1,35 @@
+
+<style>
+    /*ul.home-grid{margin-left: 10%}*/
+    ul.home-grid li{display: inline-block ; list-style: none; margin:10px 5px}
+</style>
+<div style="background-color: #efeae3; margin-top: -5px">
+    <div class="container" style="margin-top: 4%">
+        <div class="row clearfix">
+		<div class="col-md-12 column">
+			<div class="row">
+                            <ul class="home-grid">
+                                <?php foreach ($query->result() as $row): ?>
+                                <li>
+                                    <a  href="<?php echo base_url() ?>report/<?=$row->item ?>/<?=$row->id ?>" class="btn btn-lg btn-warning view-report"><span class="glyphicon glyphicon-list-alt"></span> <br/>
+                                    <?=$row->item ?><br/>
+                                     <small>Click here for see report</small>
+                                    </a>
+                                </li>
+                                <?php endforeach; ?>
+                            </ul>
+                    </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div style="text-align: center; position: relative; color: orange; margin-top:0px; background-color: #ddd; padding: 10px; min-height: 300px">
+    <h2><i class="glyphicon glyphicon-th-list"></i>&nbsp;&nbsp;Please select a product to see stock report</h2>
+	<br /><br /><br /><br />
+	<a href="http://www.trycatchclasses.com/" target="_blank"; style="color:red";></h5>By TRYCATCH CLASSES<h5></a>
+</div>
+
+
+
+       <?php // echo $this->pagination->create_links();  ?>
